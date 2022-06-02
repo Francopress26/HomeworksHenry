c6e1bf157b85675abe415125b8a74e47cedb8b89
Franco Pressenda <francopress26@hotmail.com>
2022-06-02 18:58:36 -0300

    Mas hw

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -46,36 +46,40 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-    
-  let aux;
-  let cont=                             //           i     j
-  for (let i = 2; i < array.length; i++) { //[5,1,4,2,8]
-      for (let j = array.length; j > array.length-1; j--) {
-         if(array[i]<array[i-1]){
-           aux=array[i]
-           array[i]=array[i-i]
-           array[i]=aux
-         }else{
-           
-           while(array[i]>array[i]){
-              i--
-           }
-         }
-        
-      }
-    
-  }
+      //Empiezo en el segundo termino para tener con q comparar a la izq
+    for (let i = 1; i < array.length; i++) {
+     let j =i-1;// Con j voy a comparar i
+     let aux=array[i]; // "Me guardo" i 
+     while (j>=0 && array[j]>aux) { // Si j es mayor a 0, es decir que luego de restarle 1 no se haya ido de rango del array y el array[j] es mayor al auxiliar, entonces los swapeo
+       array[j+1]= array[j] // piso el valor de i con el de j, pero no importa porque i lo guarde en aux
+       j-- // resto j para que siga preguntando hacia la izquierda, hasta q encuentre un numero mas chico o se salga de rango del array, lo que significaria que no hay un numero mas chico
+     }
+     // Cuando salgo del while a array[j+1] le asigno el auxiliar que me habia guardado, porque si salio del while significa que ahi seria su lugar
+      array[j+1]=aux
+      // Despues repito todo hasta terminar de recorrer el array
+    }
+    return array
 }
-
 
 function selectionSort(array) {
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i < array.length; i++) {
+      let min = i;
+      for (let j = i+1; j < array.length; j++) {
+        if(array[j]<array[min]){
+        min=j
+      }
+    
+  }
+  let aux=array[i];
+  array[i]=array[min]
+  array[min]=aux
 }
-
+return array
+}
 
 // No modificar nada debajo de esta línea
 // --------------------------------
--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,18 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  let pivot = [array[0]] // Pos 0: 5
+  let arrMay = []
+  let arrMen= []
+    for (let i = 0; i < array.length; i++) {
+          if(array[i]>pivot[0] && array.length>1){
+            arrMay.push[array[i]]
+          }else{
+            arrMen.push[array[i]]
+          }
+      quickSort(array)
+    }
+  return array
 }
 
 function mergeSort(array) {
